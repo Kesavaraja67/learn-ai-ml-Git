4f0dd70188b817b108c51ac826ef41c56b733a18
Kesavaraja M <krkesavaraja@gmail.com>
2025-08-01 19:58:51 +0530

    Update : String methods added

--- app.py
+++ app.py
@@ -29,3 +29,20 @@ full_name = f"{first_name} {last_name}"  # f-string
 print(full_name)
 full_name = f"{len(first_name)} {3 + 3}"
 print(full_name)
+
+# Strings methods
+course = "  python programming  "
+print(course.upper())  # prints in uppercase
+print(course.lower())  # prints in lowercase
+print(course.title())  # prints in title case
+print(course.lstrip())  # removes leading spaces(left side empty spaces)
+print(course.rstrip())  # removes trailing spaces(right side empty spaces)
+print(course.find("pro"))
+print(course.find("Pro"))
+print(course.replace("p", "j"))
+print(course.replace("python", "java"))
+# checks if the string contains the substring(gives answer in boolean)
+print("pro" in course)
+print("right" not in course)
+
+# just testing if Git tracks this
