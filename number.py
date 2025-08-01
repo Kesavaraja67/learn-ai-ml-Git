04c807b569a24ecd06752741097ff4651eeb3994
Kesavaraja M <krkesavaraja@gmail.com>
2025-08-02 00:08:39 +0530

    Initial commit numbers.py file added

---
+++ number.py
@@ -0,0 +1,17 @@
+import math
+print(10 + 3)  # addition
+print(10 - 3)  # subtraction
+print(10 * 3)  # multiplication
+print(10 / 3)  # division
+print(10 // 3)  # floor division
+print(10 % 3)  # reminder
+print(10 ** 3)  # exponentiation
+
+x = 10
+x = 10 + 3  # both gives same result
+x += 3  # same as above
+
+print(round(3.3))
+print(abs(-9.5))
+
+print(math.ceil(5.5))
